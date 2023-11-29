909dab400efba061aa4c7640a9881975db78ddb7
mmzk1526 <yc4120@ic.ac.uk>
2023-11-29 21:12:53 +0000

    [mmzk] (refactor) remove an unused function

--- lib/utilities/prelude.dart
+++ lib/utilities/prelude.dart
@@ -1,14 +1,3 @@
-/// A collection of useful functions that are not specific to any one domain.
-class Prelude {
-  /// Equivalent to `x == null ? null : f(x)`, or `x?.let(f)` in Kotlin syntax.
-  static S? liftNull<T, S>(S Function(T) f, T? x) {
-    if (x == null) {
-      return null;
-    }
-    return f(x);
-  }
-}
-
 /// Generic mutable pair class.
 class Pair<T1, T2> {
   T1 first;
